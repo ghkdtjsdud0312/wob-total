09b4f5ce3af689ed6c81d5068422dc2c51c22237
songwoohee <songwoohee@naver.com>
2023-12-11 14:06:46 +0900

    Repository 일부 추가(post,postimage,category)

---
+++ src/main/java/com/kh/wob/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.kh.wob.repository;
+
+import com.kh.wob.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/kh/wob/repository/PostImageRepository.java
@@ -0,0 +1,7 @@
+package com.kh.wob.repository;
+
+import com.kh.wob.entity.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+}
