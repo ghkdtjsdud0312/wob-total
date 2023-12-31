9bf3a1b92605a7de415ad38b2c05a7a203dea9a2
HyunSu1210 <hyunsu015@gmail.com>
2023-12-31 22:56:06 +0900

    설정 - 내가 작성한 글 항목 추가

--- src/main/react/src/page/Setting/Setting.jsx
+++ src/main/react/src/page/Setting/Setting.jsx
@@ -45,43 +45,51 @@ const Setting = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <SettingHeader title="환경설정" />
-      <Container>
-        <SettingBtn onClick={() => navigate("/Account")}>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/person.png?alt=media&token=18faf370-c600-429b-acfe-a9202ec49dee"
-            alt="계정관리"
-          />
-          <span className="text">계정관리</span>
-          <span className="pointer">&gt;</span>
-        </SettingBtn>
-        <SettingBtn onClick={() => navigate("/Questions")}>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/question.png?alt=media&token=90479a46-00a2-4e88-b367-c22a1acd6c10"
-            alt="문의하기"
-          />
-          <span className="text">문의하기</span>
-          <span className="pointer">&gt;</span>
-        </SettingBtn>
-        <SettingBtn onClick={() => navigate("/PaymentDetails")}>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/won.png?alt=media&token=6e1058ae-9f43-4c10-add5-6e2f1a79531e"
-            alt="결제내역"
-          />
-          <span className="text">결제내역</span>
-          <span className="pointer">&gt;</span>
-        </SettingBtn>
-        <SettingBtn onClick={() => navigate("/Policy")}>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/insurance.png?alt=media&token=09236ab0-25ed-41b6-b0d5-ea45e88b87e4"
-            alt="정책및약관"
-          />
-          <span className="text">정책 및 약관</span>
-          <span className="pointer">&gt;</span>
-        </SettingBtn>
-      </Container>
-    </>
+      <>
+        <SettingHeader title="환경설정" />
+        <Container>
+          <SettingBtn onClick={() => navigate("/Account")}>
+            <img
+                src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/person.png?alt=media&token=18faf370-c600-429b-acfe-a9202ec49dee"
+                alt="계정관리"
+            />
+            <span className="text">계정관리</span>
+            <span className="pointer">&gt;</span>
+          </SettingBtn>
+          <SettingBtn onClick={() => navigate("/")}>
+            <img
+                src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/list-items.png?alt=media&token=53de6fde-d323-4dc5-9877-c6b6adc6970c"
+                alt="내가 작성한 글"
+            />
+            <span className="text">내가 작성한 글</span>
+            <span className="pointer">&gt;</span>
+          </SettingBtn>
+          <SettingBtn onClick={() => navigate("/Questions")}>
+            <img
+                src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/question.png?alt=media&token=90479a46-00a2-4e88-b367-c22a1acd6c10"
+                alt="문의하기"
+            />
+            <span className="text">문의하기</span>
+            <span className="pointer">&gt;</span>
+          </SettingBtn>
+          <SettingBtn onClick={() => navigate("/PaymentDetails")}>
+            <img
+                src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/won.png?alt=media&token=6e1058ae-9f43-4c10-add5-6e2f1a79531e"
+                alt="결제내역"
+            />
+            <span className="text">결제내역</span>
+            <span className="pointer">&gt;</span>
+          </SettingBtn>
+          <SettingBtn onClick={() => navigate("/Policy")}>
+            <img
+                src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/insurance.png?alt=media&token=09236ab0-25ed-41b6-b0d5-ea45e88b87e4"
+                alt="정책및약관"
+            />
+            <span className="text">정책 및 약관</span>
+            <span className="pointer">&gt;</span>
+          </SettingBtn>
+        </Container>
+      </>
   );
 };
 export default Setting;
