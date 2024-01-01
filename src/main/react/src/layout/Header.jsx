0ffbe5885e0dfa6bfa608f312331958f95568fce
songwoohee <songwoohee@naver.com>
2024-01-01 15:42:00 +0900

    header-로그인시 환영인사와 함께 닉네임 헤더 오른편에 확인 가능

--- src/main/react/src/layout/Header.jsx
+++ src/main/react/src/layout/Header.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import MyPageAxiosApi from "../api/MyPageAxiosApi";
 
 const Container = styled.div`
   display: flex;
@@ -31,13 +32,29 @@ const LogoImage = styled.img`
   margin: 10px;
 `;
 
+const RightBox = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
 const SearchIcon = styled(FontAwesomeIcon)`
   cursor: pointer;
   color: #04bf8a;
   font-size: 30px;
 `;
 
+const GreetingText = styled.span`
+  margin-right: 10px;
+  padding-top: 13px;
+  font-weight: bold;
+  color: #555555;
+`;
+
 const Header = () => {
+  const [user, setUser] = useState(null);
+
   const logoImage =
     "https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/logosmall.png?alt=media&token=5f1756d7-08ab-4930-a834-1c2d82e2c34d";
 
@@ -51,12 +68,33 @@ const Header = () => {
     navigate("searchMain");
   };
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      const email = localStorage.getItem("email");
+      if (email) {
+        const rsp = await MyPageAxiosApi.userGetOne(email);
+        if (rsp.status === 200) {
+          setUser(rsp.data);
+          console.log("user 정보 잘 왔나 : ", rsp.data);
+        }
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Container>
         <HeaderBox>
           <LogoImage src={logoImage} alt="logo" onClick={goToHome} />
-          <SearchIcon icon={faSearch} onClick={goToSearchPage} />
+          <RightBox>
+            {user ? (
+              <GreetingText>안녕, 내친구 {user.nickname}</GreetingText>
+            ) : (
+              <GreetingText>로그인 해주세요</GreetingText>
+            )}
+            <SearchIcon icon={faSearch} onClick={goToSearchPage} />
+          </RightBox>
         </HeaderBox>
       </Container>
     </>
